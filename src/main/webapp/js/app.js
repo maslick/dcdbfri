463d51d4d8d88be06e226a3dbeb54c37e643c9cb
Pavel Maslov <pavel.maslov@cosylab.com>
2015-08-31 00:12:55 +0200

    Added Snarl.js and Waves.js for notifications

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,28 +1,33 @@
 (function() {
     var app = angular.module('dcdb', []);
 
-    app.controller('dcdbController', ['$http', '$scope', function($http, $scope) {
+    app.controller('dcdbController', ['$http', '$scope', '$window', function($http, $scope, $window) {
         var thisclass = this;
-
-        $scope.show_alert = false;
-        thisclass.iocs = []
-
-        this.status = "";
+        thisclass.iocs = [];
         this.newioc = {};
+
+        this.notify = function(title, text, icon) {
+            var Snarl = $window.Snarl;
+            Snarl.addNotification({
+                title: title,
+                text: text,
+                icon: '<i class="' + icon + '"></i>'
+            });
+        };
 
         this.updateIOCtable = function() {
             $http.get('http://' + location.host + '/wildfly-helloworld/dcdb/iocs').success(function (data) {
                 console.log(data);
                 thisclass.iocs = data;
             });
-        }
+        };
+
         this.updateIOCtable();
 
         this.delete = function(id) {
             $http.delete('http://' + location.host + '/wildfly-helloworld/dcdb/iocs/delete/' + id ).success(function (data){
                 console.log(id + " was deleted from database!");
-                $scope.show_alert = true;
-                thisclass.status = id + " was deleted from database!";
+                thisclass.notify("DELETE", "IOC #" + id + " was deleted from database.", "glyphicon glyphicon-remove");
                 thisclass.updateIOCtable();
             });
 
@@ -31,8 +36,8 @@
         this.submitIoc = function() {
             $http.post('http://' + location.host + '/wildfly-helloworld/dcdb/iocs/add', this.newioc, []).success(function (data) {
                 console.log("IOC added!");
+                thisclass.notify("ADD", "New IOC added", "glyphicon glyphicon-plus");
                 thisclass.updateIOCtable();
-                $scope.show_alert = false;
                 thisclass.newioc = {};
             });
         };
