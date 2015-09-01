2c4a0d942ba216c2e5944eda0c5ec42108096674
Pavel Maslov <pavel.maslov@cosylab.com>
2015-09-01 11:40:14 +0200

    added a db configuration class

---
+++ src/main/java/org/jboss/as/quickstarts/helloworld/models/DbConfig.java
@@ -0,0 +1,46 @@
+package org.jboss.as.quickstarts.helloworld.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by maslick on 01/09/15.
+ */
+public class DbConfig {
+    /* Reads environmental variables for persistence configuration */
+    public static Map<String, Object> getPersistConfig() {
+        Map<String, String> env = System.getenv();
+        Map<String, Object> configOverrides = new HashMap<String, Object>();
+
+        for (String envName : env.keySet()) {
+            if (envName.contains("DB_HOSTURL")) {
+                /* DB_URL = jdbc:mysql://localhost:3306/dcdbfri
+                *  DB_HOSTURL = localhost
+                * */
+
+                configOverrides.put("javax.persistence.jdbc.url", "jdbc:mysql://" + env.get(envName) + ":3306/dcdbfri");
+            }
+            if (envName.contains("DB_USER")) {
+                configOverrides.put("javax.persistence.jdbc.user", env.get(envName));
+            }
+            if (envName.contains("DB_PWD")) {
+                configOverrides.put("javax.persistence.jdbc.password", env.get(envName));
+            }
+        }
+
+        /* For Docker environment */
+        String host = System.getenv("MYSQL_PORT_3306_TCP_ADDR");
+        String port = System.getenv("MYSQL_PORT_3306_TCP_PORT");
+        if (host != null && port != null) {
+            configOverrides.put("javax.persistence.jdbc.url", "jdbc:mysql://" + host + ":" + port + "/dcdbfri");
+        }
+
+        String user = System.getenv("MYSQL_ENV_MYSQL_USER");
+        String pwd = System.getenv("MYSQL_ENV_MYSQL_PASSWORD");
+        if (user != null && pwd != null) {
+            configOverrides.put("javax.persistence.jdbc.user", user);
+            configOverrides.put("javax.persistence.jdbc.password", pwd);
+        }
+        return configOverrides;
+    }
+}
--- src/main/java/org/jboss/as/quickstarts/helloworld/rest/restService.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/rest/restService.java
@@ -1,5 +1,6 @@
 package org.jboss.as.quickstarts.helloworld.rest;
 
+import org.jboss.as.quickstarts.helloworld.models.DbConfig;
 import org.jboss.as.quickstarts.helloworld.models.IOC;
 
 
@@ -21,55 +22,15 @@ import java.util.Map;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class restService {
-
-    private Map<String, Object> persistConfig() {
-        Map<String, String> env = System.getenv();
-        Map<String, Object> configOverrides = new HashMap<String, Object>();
-
-        for (String envName : env.keySet()) {
-            if (envName.contains("DB_HOSTURL")) {
-                /* DB_URL = jdbc:mysql://localhost:3306/dcdbfri
-                *  DB_HOSTURL = localhost
-                * */
-
-                configOverrides.put("javax.persistence.jdbc.url", "jdbc:mysql://" + env.get(envName) + ":3306/dcdbfri");
-            }
-            if (envName.contains("DB_USER")) {
-                configOverrides.put("javax.persistence.jdbc.user", env.get(envName));
-            }
-            if (envName.contains("DB_PWD")) {
-                configOverrides.put("javax.persistence.jdbc.password", env.get(envName));
-            }
-        }
-
-        /* For Docker environment */
-        String host = System.getenv("MYSQL_PORT_3306_TCP_ADDR");
-        String port = System.getenv("MYSQL_PORT_3306_TCP_PORT");
-        if (host != null && port != null) {
-            configOverrides.put("javax.persistence.jdbc.url", "jdbc:mysql://" + host + ":" + port + "/dcdbfri");
-        }
-
-        String user = System.getenv("MYSQL_ENV_MYSQL_USER");
-        String pwd = System.getenv("MYSQL_ENV_MYSQL_PASSWORD");
-        if (user != null && pwd != null) {
-            configOverrides.put("javax.persistence.jdbc.user", user);
-            configOverrides.put("javax.persistence.jdbc.password", pwd);
-        }
-
-        return configOverrides;
-    }
-
     private static final String PERSISTENCE_UNIT_NAME = "dcdbfri";
-    private EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME, persistConfig());
+    private EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME, DbConfig.getPersistConfig());
     private EntityManager em = factory.createEntityManager();
-
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response listIOCs() {
         TypedQuery<IOC> query  = em.createNamedQuery("IOC.findAll", IOC.class);
         List<IOC> iocs = query.getResultList();
-
         return Response
                 .status(Response.Status.OK)
                 .header("Access-Control-Allow-Origin", "*")
